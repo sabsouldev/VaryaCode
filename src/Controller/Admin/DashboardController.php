6daa5a583e0822201ffa57085fa77c8ac92b9ef6
soulagnet Sabrina <soulagnetsabrina@gmail.com>
2026-03-20 10:05:57 +0100

    dashboard admin

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,17 +2,20 @@
 
 namespace App\Controller\Admin;
 
-use App\Entity\ContactMessage;
+
 use App\Entity\DevisRequest;
-use App\Entity\Project;
-use App\Entity\Testimonial;
 use App\Repository\DevisRequestRepository;
 use EasyCorp\Bundle\EasyAdminBundle\Attribute\AdminDashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use App\Controller\Admin\ProjectCrudController;
+use App\Controller\Admin\TestimonialCrudController;
+use App\Controller\Admin\DevisRequestCrudController;
+use App\Controller\Admin\ContactMessageCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+
 
 #[AdminDashboard(routePath: '/admin', routeName: 'admin')]
 class DashboardController extends AbstractDashboardController
@@ -21,7 +24,7 @@ class DashboardController extends AbstractDashboardController
     {
     }
 
-    #[Route('/admin', name: 'admin')]
+    
     public function index(): Response
     {
         return $this->render('admin/dashboard.html.twig', [
@@ -43,11 +46,11 @@ class DashboardController extends AbstractDashboardController
     {
         yield MenuItem::linkToDashboard('Tableau de bord', 'fa fa-home');
         yield MenuItem::section('Contenu');
-        yield MenuItem::linkToCrud('Portfolio', 'fa fa-images', Project::class);
-        yield MenuItem::linkToCrud('Témoignages', 'fa fa-quote-right', Testimonial::class);
+        yield MenuItem::linkTo(ProjectCrudController::class, 'Portfolio', 'fa fa-images')->setAction(Action::INDEX);
+        yield MenuItem::linkTo(TestimonialCrudController::class, 'Témoignages', 'fa fa-quote-right')->setAction(Action::INDEX);
         yield MenuItem::section('Demandes');
-        yield MenuItem::linkToCrud('Demandes de devis', 'fa fa-file-invoice', DevisRequest::class);
-        yield MenuItem::linkToCrud('Messages', 'fa fa-envelope', ContactMessage::class);
+        yield MenuItem::linkTo(DevisRequestCrudController::class, 'Demandes de devis', 'fa fa-file-invoice')->setAction(Action::INDEX); 
+        yield MenuItem::linkTo(ContactMessageCrudController::class, 'Messages', 'fa fa-envelope')->setAction(Action::INDEX);
         yield MenuItem::section('');
         yield MenuItem::linkToRoute('Voir le site', 'fa fa-external-link-alt', 'app_home');
         yield MenuItem::linkToLogout('Déconnexion', 'fa fa-sign-out-alt');
